99b8c67d0102124bb9c5a35a2e3f9d94197c57ea
boomphxmx47 <phumiphat6113@email.com>
2026-05-06 17:44:59 +0700

    fix unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useReducer } from "react";
+import { useState } from "react";
 
 // ─── THEME ────────────────────────────────────────────────────────────────────
 const C = {
